eaf37dc943c39f662511a45c030af8e67ffafb8b
Francesco Randi <francesco.randi@gmail.com>
2023-01-09 15:06:55 -0500

    updated docs of module

--- wormneuroatlas/__init__.py
+++ wormneuroatlas/__init__.py
@@ -1,3 +1,22 @@
+'''
+Neural signal propagation atlas [1], genome [2], and single-cell 
+transcriptome [3], neuropeptide/GPCR deorphanization [4], anatomical 
+connectome [5,6], and monoaminergic connectome [7] all in one place. Worm Neuro 
+Atlas allows to build a basic version of the neuropeptidergic connectome [8] 
+([8] contains more detailed analysis). Please cite the code if you use it, along
+with the papers containing the datasets you use.
+
+
+1. Randi et al., arXiv 2022 https://arxiv.org/abs/2208.04790
+2. WormBase, wormbase.org
+3. Taylor et al., Cell 2021
+4. Beets et al., bioRxiv 2022 https://www.biorxiv.org/content/10.1101/2022.10.30.514428v1
+5. White et al., Phil. Trans. R. Soc 1986
+6. Witvliet et al., Nature 2021
+7. Bentley et al., PLOS Comp. Bio. 2016
+8. Ripoll-Sanchez et al., bioRxiv 2022 https://www.biorxiv.org/content/10.1101/2022.10.30.514396v2
+'''
+
 from .NeuroAtlas import NeuroAtlas
 try:
     from .WormBase import WormBase
